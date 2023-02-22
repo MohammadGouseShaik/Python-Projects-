b246698f094040ebde351af3ee0bffd1a4503c41
Mohammad Gouse Shaik <105580146+MohammadGouseShaik@users.noreply.github.com>
2023-02-22 23:20:49 +0530

    Add files via upload

---
+++ Age Calculator.py
@@ -0,0 +1,9 @@
+import datetime
+def calculate_age(birth_year):
+    current_year = datetime.datetime.now().year
+    age = current_year - birth_year
+    return age
+
+birth_year = int(input("Enter your birth year: "))
+age = calculate_age(birth_year)
+print("Your age is:", age)
